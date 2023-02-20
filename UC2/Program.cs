6ebc04ab49af97725360db529c0593b9c8547cfc
prathy08 <bashaboinaprathyusha08@gmail.com>
2023-02-20 09:58:07 +0530

    Add:game

---
+++ UC2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace UC2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0, rollDice;
+            int playerOne;
+            playerOne = position;
+            Random random = new Random();
+            rollDice = random.Next(1, 7);
+            Console.WriteLine("....Snake And Ladder....");
+            Console.WriteLine("position of the playerone is =" + playerOne);
+            Console.WriteLine("Rolled dice number is:" + rollDice);
+            Console.ReadLine();
+        }
+    }
+    
+}
