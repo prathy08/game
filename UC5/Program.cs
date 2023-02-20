a98472321f5e7b0a008f5614cd985a8da06207ef
prathy08 <bashaboinaprathyusha08@gmail.com>
2023-02-20 10:21:39 +0530

    Add:game

---
+++ UC5/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace UC5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Snake = 0;
+            const int Ladder = 1;
+            const int NoGame = 2;
+            int position = 0;
+            Random random = new Random();
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine("Rolled Dice number is:" + diceRoll);
+            int choice = random.Next(0, 3);
+
+
+            switch (choice)
+            {
+                case Snake:
+                    position -= diceRoll;
+                    Console.WriteLine("Its a snake");
+                    if (position < 0)
+                    {
+                        Console.WriteLine("Player 1 position : 0");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player 1 position : " + position);
+                    }
+                    break;
+                case Ladder:
+                    position += diceRoll;
+                    Console.WriteLine("Its a ladder");
+                    Console.WriteLine("Player 1 position : " + position);
+                    break;
+                case NoGame:
+                    Console.WriteLine("Its a NoGame");
+                    Console.WriteLine("Player 1 position : " + position);
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
